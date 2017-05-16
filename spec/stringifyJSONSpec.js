26a9e541301a0830d98d55dbaca343aace544921
Hack Reactor Student <student@hackreactor.com>
2017-05-15 18:36:07 -0700

    Modified tests

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -3,15 +3,16 @@ describe('stringifyJSON', function() {
   it('should match the result of calling JSON.stringify', function() {
 
     stringifiableObjects.forEach(function(test) {
+      //let test  = '{a: 1, b: 2}';
       var expected = JSON.stringify(test);
       var result = stringifyJSON(test);
-      expect(result).to.equal(FILL_ME_IN);
+      expect(result).to.equal(expected);
     });
 
     unstringifiableValues.forEach(function(obj) {
       var expected = JSON.stringify(obj);
       var result = stringifyJSON(obj);
-      expect(result).to.equal(FILL_ME_IN);
+      expect(result).to.equal(expected);
     });
 
   });
